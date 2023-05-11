e18342669dc4af874b6f43323fb9f61ebcf39783
Bikash Kumar Singha <103056312+bikashkumarsingha@users.noreply.github.com>
2023-05-11 12:30:51 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -4,7 +4,7 @@ First = float(input("Enter the first number:"))
 Second = float(input("Enter the second number here:"))
 
 print(
-  "Press 1 for Addition + \nPress 2 for Substraction - \nPress 3 for Multiplication * \nPress 4 for Division \nPress 5Modules %"
+  "Press 1 for Addition + \nPress 2 for Substraction - \nPress 3 for Multiplication * \nPress 4 for Division \nPress 5 for Modules %"
 )
 
 choice = int(input("Press 1-4 given above ="))
